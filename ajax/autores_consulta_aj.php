47729f17fc51c7892b5e8af9dd7bb51c55883f60
Marcelo Vilara <marcelovilara@gmail.com>
2016-08-28 18:51:14 -0300

    Distinct autor

--- ajax/autores_consulta_aj.php
+++ ajax/autores_consulta_aj.php
@@ -1,7 +1,7 @@
 <?php
 
     $pdo = new PDO("mysql:host=186.202.152.194; dbname=ictiomadeira10; charset=utf8;", "ictiomadeira10", "guariba***", $opcoes);
-    $dados = $pdo->prepare("SELECT DISTINCT * FROM autor");
+    $dados = $pdo->prepare("SELECT * FROM autor");
     $dados->execute();
     echo json_encode($dados->fetchAll(PDO::FETCH_ASSOC));
 
